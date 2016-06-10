4200d4af49a2d3e847f2c531087770a7830ed26e
olivierdrg <olivierdrogue@gmail.com>
2016-06-10 12:22:15 +0200

    sql

--- public/INFO/minichat.sql
+++ public/INFO/minichat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 10 Juin 2016 à 11:04
+-- Généré le: Ven 10 Juin 2016 à 12:21
 -- Version du serveur: 5.6.28-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.14
 
@@ -23,17 +23,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `author`
+--
+
+CREATE TABLE IF NOT EXISTS `author` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(15) NOT NULL,
+  `pseudo` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `message`
 --
 
 CREATE TABLE IF NOT EXISTS `message` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `id_author` int(11) unsigned NOT NULL,
-  `pseudo` varchar(15) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` varchar(1023) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`id`, `id_author`, `date`, `content`) VALUES
+(1, 1, '2016-06-10 09:26:17', 'Il fait beau ce matin !'),
+(2, 2, '2016-06-10 09:26:17', 'Super génial le chat'),
+(3, 1, '2016-06-10 09:26:17', 'Tu fais quoi ce weekend ?'),
+(4, 2, '2016-06-10 09:26:17', 'Ben euh jsais pas...'),
+(5, 1, '2016-06-10 09:26:17', 'Moi non plus');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
