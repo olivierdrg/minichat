959bde3d7781ec448203446d9639be9cdb951c8d
olivierdrg <olivierdrogue@gmail.com>
2016-06-10 11:05:56 +0200

    sql

--- public/INFO/minichat.sql
+++ public/INFO/minichat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 10 Juin 2016 à 10:20
+-- Généré le: Ven 10 Juin 2016 à 11:04
 -- Version du serveur: 5.6.28-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.14
 
@@ -28,7 +28,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `message` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) unsigned NOT NULL,
+  `id_author` int(11) unsigned NOT NULL,
+  `pseudo` varchar(15) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` varchar(1023) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
